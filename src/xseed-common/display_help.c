372865b3ae83050b14f6d329165981005ee6830d
Dylan Thies <dylanthies@isti.com>
2018-05-18 14:41:34 -0400

    external libmseed seems to be building reliably now

---
+++ src/xseed-common/display_help.c
@@ -0,0 +1,13 @@
+#include <xseed-common/config.h>
+#include <stdio.h>
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#include <xseed-common/vcs_getopt.h>
+#else
+#include <unistd.h>
+#include <getopt.h>
+#endif
+
+void display_help (char *program_name, char *message, struct option long_opts, char **opt_descriptions)
+{
+    printf("Usage: %s\n", program_name);
+}
