03be2e1f0c5dd470e32db6b7e5351631c7cf1477
Dylan Thies <dylanthies@isti.com>
2018-06-22 11:34:20 -0400

    adding unix functions for windows

---
+++ src/xseed-common/string.h
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+
+//this is so these functions are availible to windows
+extern size_t strnlen(const char *, size_t);
+extern char *strndup(const char *, size_t)
---
+++ src/xseed-common/unix_functions_for_windows.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+size_t strnlen(const char *s, size_t max_len)
+{
+    size_t len = 0;
+    for (; len < max_len && 0 != s[len]; len);
+    return len;
+}
+
+char *strndup(const char *s, size_t n)
+{
+    size_t actual_len = strnlen(s, n);
+    char * out = (char *) calloc(actual_len + 1, sizeof(char));
+    memcpy(out, s, actual_len);
+    return out;
+};
+#endif
