0c9b11d5c97eb9cc77a0411ab68cbfc3643ed95a
Dylan Thies <dylanthies@isti.com>
2018-06-22 12:45:37 -0400

    more windows fixing

--- src/xseed-common/unix_functions_for_windows.c
+++ src/xseed-common/unix_functions_for_windows.c
@@ -2,12 +2,12 @@
 #include <string.h>
 #include "xseed_string.h"
 #if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
-/*size_t strnlen(const char *s, size_t max_len)
+size_t strnlen(const char *s, size_t max_len)
 {
     size_t len = 0;
     for (; len < max_len && 0 != s[len]; len);
     return len;
-}*/
+}
 
 char *strndup(const char *s, size_t n)
 {
